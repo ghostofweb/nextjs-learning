2eddc48241768ba71ec0ac61e4414f9455c2f3d4
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-01-28 13:05:40 +0530

    Implement user registration and email verification functionality

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,1 +1,47 @@
 import { connectDb } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { stat } from "fs";
+import {NextRequest,NextResponse} from "next/server";
+import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
+connectDb();
+
+export async function POST(req: NextRequest) {
+    try {
+        const reqBody = await req.json() //Get the request body need to put await because it is a promise 
+        const {username,email,password} = reqBody;
+        //Validate the request body
+        console.log(reqBody);
+        const user = await User.findOne({email});
+        if(user){
+            return NextResponse.json({
+                status: "error",
+                message: "User already exists"
+            }),
+            {status: 400}
+        }
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password,salt);
+        const newUser = new User({
+            username,
+            email,
+            password: hashedPassword
+        })
+        const savedUser = await newUser.save();
+        await sendEmail({email,emailType:"Verify",userId:savedUser._id});
+        //Send email to user to verify email address 
+        return NextResponse.json({
+            status: "success",
+            message: "User created successfully",
+            savedUser
+        },
+        {status: 201})
+        
+    } catch (error:any) {
+        return NextResponse.json({
+            status: "error",
+            message: error.message,
+            
+        })
+    }
+}
--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,25 +1,41 @@
+import User from '@/models/userModel';
 import nodemailer from 'nodemailer';
-
+import bcryptjs from 'bcryptjs';
 export const sendEmail = async ({email,emailType,userId}:any) => {
     try {
-        const transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false, // true for port 465, false for other ports
-            auth: {
-              user: "maddison53@ethereal.email",
-              pass: "jn7jnAPss4f63QBp6D",
-            },
-          });
+      const hashedToken = await bcryptjs.hash(userId.toString(),10)
+      if (emailType === "Verify") {
+        await User.findByIdAndUpdate(userId,
+          {verifyToken: hashedToken,verifyTokenExpire: Date.now() + 3600000}
+        )
+      } else if(emailType === "Reset") {
+        await User.findByIdAndUpdate(userId,
+          {forgotPassowrdToken: hashedToken,forgotPasswordTokenExpiry: Date.now() + 3600000}
+        )
+
+      }
+        // Looking to send emails in production? Check out our Email API/SMTP product!
+            var transport = nodemailer.createTransport({
+              host: "sandbox.smtp.mailtrap.io",
+              port: 2525,
+              auth: {
+                user: "080288ddebe231",
+                pass: "94b1ac827e9cc3"
+              }
+            });
 
           const mailOptions = {
             from: `sahiljeet singh kalsi`, // sender address
             to: email, // list of receivers
             subject: emailType === "Verify" ? "Verify your Email":"Reset your password",
-            html: "<b>Hello world?</b>", // html body
+            html: `<p>
+            Click <a href="${process.env.DOMAIN}/verifyemail?token=${hashedToken}">here</a> to ${emailType === "Verify" ? "Verify your Email":"Reset your password"}
+            or copy and paste the following link in your browser. ${process.env.DOMAIN}/verifyemail?token=${hashedToken}
+            <br>
+            </p>`, 
           }
 
-          const mailResponse = await transporter.sendMail(mailOptions);
+          const mailResponse = await transport.sendMail(mailOptions);
           return mailResponse;
     } catch (error:any) {
         throw new Error(error.message);
