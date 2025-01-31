24d4f26c62384dd71197560d18e2a5ac2ab91fb2
Sahiljeet singh kalsi <sahiljeetsinghkalsi@gmail.com>
2025-01-31 19:47:03 +0530

    Refactor email verification route for improved code readability and consistency

--- src/app/api/users/verifyemail/route.ts
+++ src/app/api/users/verifyemail/route.ts
@@ -3,25 +3,24 @@ import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 
 connectDb();
-export async function POST(req:NextRequest){
+export async function POST(req: NextRequest) {
     try {
         const reqBody = await req.json();
-        const {token} = reqBody;
+        const { token } = reqBody;
         console.log(token);
-        const user = await User.findOne({verifyToken:token,verifyTokenExpiry:{$gt:Date.now()}})
+        const user = await User.findOne({ verifyToken: token, verifyTokenExpiry: { $gt: Date.now() } })
         if (!user) {
-            return NextResponse.json({status:"error",message:"Invalid or expired token"},{status:400})
+            return NextResponse.json({ status: "error", message: "Invalid or expired token" }, { status: 400 })
         }
         console.log(user);
-        user.isVerified = true; // Update user's isVerified field
-        user.verifyToken = undefined; // Remove token from user document after verification is complete 
-        user.verifyTokenExpire = undefined; // Remove token expiry date from user document after verification is complete`
-        
-        await user.save(); // Save updated user document, also await to ensure save completes
-        return NextResponse.json({status:"success",message:"Email verified successfully"},{status:200})
-        
+        user.isVerified = true; // Update user's isVerified  field
+        user.verifyToken = undefined; // Remove token from   user document after verification is complete 
+        user.verifyTokenExpire = undefined; // Remove token  expiry date from user document after verification is complete
+        await user.save(); // Save updated user document,    also await to ensure save completes
+        return NextResponse.json({ status: "success", message: "Email verified successfully" }, { status: 200 })
 
-    } catch (error:any) {
-        return NextResponse.json({status:"error",message:error.message},{status:500})
+
+    } catch (error: any) {
+        return NextResponse.json({ status: "error", message: error.message }, { status: 500 })
     }
 }
